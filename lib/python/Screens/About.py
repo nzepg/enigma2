918d3047dd56a24078134d6349d1127ecba385a3
Huevos <rowland@moralet.com>
2025-06-07 15:43:07 +0200

    [SystemMemoryInfo] update self.skinName

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -428,7 +428,6 @@ class SystemMemoryInfo(AboutBase):
 	def __init__(self, session):
 		AboutBase.__init__(self, session, labels=True)
 		self.setTitle(_("Memory"))
-		self.skinName = ["AboutOE", "SystemMemoryInfo"]
 		out_lines = open("/proc/meminfo").readlines()  # output is in kiB so multiply by 1024
 		self.AboutText = self.addColor(_("RAM")) + "\n"
 		for lidx in range(len(out_lines) - 1):
